fc6ff985632df44b0be6cba62841bfa119048b70
Adrián Da Silva <adriandasilva23@hotmail.com>
2024-05-30 19:58:50 -0300

    Función para agregar productos arreglada. Función para eliminar producto del carrito implementada.

--- scripts/classes/shoppingCart.js
+++ scripts/classes/shoppingCart.js
@@ -1,5 +1,7 @@
 import { saveInLocalStorage } from "../utils/localStorage.js";
 import { getProducts } from "../services/loadData.js";
+import { renderShoppingCartList } from "../components/shoppingCartList.js";
+import { emptyElement } from "../utils/dom.js";
 export class ShoppingCart{
     constructor () {
         this.productList = []
@@ -19,57 +21,43 @@ export class ShoppingCart{
     }
 
     //Añadir producto y actualizar precio
-    addProduct(id, quantity) {
-        const targetProductIndex = this.productList.findIndex(product => product.id == id);
-
-        // Si el producto ya existe, actualizar cantidad y subtotal
-        if (targetProductIndex !== -1) {
-            //Actualizar cantidad y subtotal
-            this.productList[targetProductIndex].quantity += quantity;
-            this.productList[targetProductIndex].subTotal = this.productList[targetProductIndex].price * this.productList[targetProductIndex].quantity;
+    async addProduct(id, quantity) {
+        try {
+          const data = await getProducts();
+          const targetProduct = data.find(product => parseInt(product.id) === id);
+          if (targetProduct) {
+            const newProduct = { ...targetProduct, quantity, subTotal: targetProduct.price * quantity };
+            this.productList.push(newProduct);
             this.total = this.updateTotal();
 
-            //Actualizar el carrito en el localStorage
+            // Actualizar el carrito en el localStorage
             saveInLocalStorage("cart", this);
-        } else {
-            //Si el producto no existe, agregar a la lista
-            getProducts()
-                .then(data => {
-                    const targetProduct = data.find(product => parseInt(product.id) === id);
-                    if (targetProduct) {
-                        const newProduct = { ...targetProduct, quantity, subTotal: targetProduct.price * quantity };
-                        this.productList.push(newProduct);
-                        this.total = this.updateTotal();
 
-                        //Actualizar el carrito en el localStorage
-                        saveInLocalStorage("cart", this);
-                    } else {
-                        console.error("No se encontró el producto");
-                    }
-                })
-                .catch(error => console.error(error));
+            // Actualizar lista de productos en carrito
+            emptyElement(document.querySelector("#shopping-cart-container"));
+            renderShoppingCartList(this);
+          } else {
+            console.error("No se encontró el producto");
+          }
+        } catch (error) {
+          console.error(error);
         }
-    }
+      }
 
     //Eliminar producto y actualizar precio
     removeProduct(id) {
-        const targetProduct = this.findById(id);
-        console.log(targetProduct);
-        return
+        const targetProductIndex = this.productList.findIndex(product => product.id == id);
 
-        //Verificar que el producto esté en el carrito
-        if (targetProduct) {
-            const index = this.productList.indexOf(targetProduct);
-            if (index !== -1) {
-                //Eliminar
-                this.productList.splice(index, 1);
-                //Actualizar precio
-                this.total = this.updateTotal()
-                localStorage.setItem("cart", JSON.stringify(this));
-            }
-        } else {
-            alert("No se encontró el producto");
+        if (targetProductIndex != -1) {
+            this.productList.splice(targetProductIndex, 1);
+            this.total = this.updateTotal();
         }
+
+        //Actualizar el carrito en el localStorage
+        saveInLocalStorage("cart", this);
+
+        //Re-renderizar carrito
+        renderShoppingCartList(this);
     }
 
     //Editar cantidad de producto
--- scripts/components/shoppingCartList.js
+++ scripts/components/shoppingCartList.js
@@ -5,58 +5,59 @@ import { createCartItem } from "./cartItem.js";
 export function renderShoppingCartList(cart) {
     const shoppingCartContainer = document.querySelector("#shopping-cart-container");
     shoppingCartContainer.innerHTML = "<h2>Carrito de compras</h2>";
+    const createEmptyCartMessage = () => {
+        const emptyCartMessage = createElement("p", "empty-cart-message");
+        emptyCartMessage.textContent = "No hay productos en el carrito";
+        shoppingCartContainer.appendChild(emptyCartMessage);
+    }
 
-    // Crear tabla para mostrar el carrito
-    const shoppingCartList = createElement("table", "shopping-cart-list");
-    shoppingCartList.innerHTML = `
-        <tr>
-            <th></th>
-            <th>Nombre</th>
-            <th>Cantidad</th>
-            <th>Precio</th>
-            <th>Eliminar</th>
-        </tr>
-    `
+    if (cart.productList.length === 0) {
+        createEmptyCartMessage();
+    } else {
+        // Crear tabla para mostrar el carrito
+        const shoppingCartList = createElement("table", "shopping-cart-list");
+        shoppingCartList.innerHTML = `
+            <tr>
+                <th></th>
+                <th>Nombre</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+                <th>Eliminar</th>
+            </tr>
+        `
 
-    // Crear items del carrito
-    cart.productList.forEach(product => {
-        const cartItem = createCartItem(product);
-        shoppingCartList.appendChild(cartItem);
-    });
-    shoppingCartContainer.appendChild(shoppingCartList);
+        // Crear items del carrito
+        cart.productList.forEach(product => {
+            const cartItem = createCartItem(product);
+            shoppingCartList.appendChild(cartItem);
+        });
+        shoppingCartContainer.appendChild(shoppingCartList);
 
-    // Crear total del carrito
-    const totalContainer = createElement("div", "total-container");
-    totalContainer.innerHTML = `
-        <h3>Total: $${cart.total}</h3>
-    `;
-    shoppingCartContainer.appendChild(totalContainer);
+        // Crear total del carrito
+        const totalContainer = createElement("div", "total-container");
+        totalContainer.innerHTML = `
+            <h3>Total: $${cart.total}</h3>
+        `;
+        shoppingCartContainer.appendChild(totalContainer);
 
-    //Crear sección para vaciar el carrito y finalizar la compra
-    const shoppingCartActions = createElement("div", "shopping-cart-actions");
-    shoppingCartActions.innerHTML = `
-        <button class="clean-cart">Vaciar carrito</button>
-        <button class="finish-purchase">Realizar compra</button>
-    `;
-    shoppingCartContainer.appendChild(shoppingCartActions);
+        //Crear sección para vaciar el carrito y finalizar la compra
+        const shoppingCartActions = createElement("div", "shopping-cart-actions");
+        shoppingCartActions.innerHTML = `
+            <button class="clean-cart">Vaciar carrito</button>
+            <button class="finish-purchase">Realizar compra</button>
+        `;
+        shoppingCartContainer.appendChild(shoppingCartActions);
+    }
+
 
     // Evento: Click
     document.addEventListener("click", event => {
         //Vaciar carrito
         if (event.target.classList.contains("clean-cart")) {
             cart.cleanCart();
-            shoppingCartList.innerHTML = `
-                <tr>
-                    <th></th>
-                    <th>Nombre</th>
-                    <th>Cantidad</th>
-                    <th>Precio</th>
-                    <th>Eliminar</th>
-                </tr>
-            `;
-            totalContainer.innerHTML = `
-                <h3>Total: $0</h3>
-            `;
+            emptyElement(shoppingCartContainer);
+            shoppingCartContainer.innerHTML = "<h2>Carrito de compras</h2>";
+            createEmptyCartMessage();
         }
 
         //Editar cantidad de productos
@@ -82,7 +83,10 @@ export function renderShoppingCartList(cart) {
                 subTotalCell.textContent = `$${product.subTotal}`;
 
                 //Calcular nuevo total
-                //const total = cart.updateTotal();
+                console.log(cart.total)
+                cart.updateTotal();
+
+                const totalContainer = document.querySelector(".total-container");
                 totalContainer.innerHTML = `
                     <h3>Total: $${cart.total}</h3>
                 `;
@@ -91,6 +95,14 @@ export function renderShoppingCartList(cart) {
                 quantityInput.disabled = true;
                 event.target.textContent = 'Editar';
             }
+        }
+
+        //Eliminar producto
+        if (event.target.classList.contains("remove-cart-item")) {
+            const id = parseInt(event.target.dataset.id);
+            cart.removeProduct(id);
+            emptyElement(shoppingCartContainer);
+            renderShoppingCartList(cart);
         }
 
         //Finalizar compra
